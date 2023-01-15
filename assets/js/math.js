e15f13bcc28c6f35e07ab91434b926be6eba079e
Felipe Munoz <analista.felipe.m@gmail.com>
2023-01-15 01:24:54 -0300

    Clase 3 terminada.

--- assets/js/math.js
+++ assets/js/math.js
@@ -87,3 +87,52 @@
     }
 
     console.groupEnd('Cuadrado');
+
+
+/***** CLASE 3 *****/
+
+    // creo una nueva agrupacion, esta vez llamada Círculo.
+    console.group('Círculo')
+
+    // creo una constante a la que le asigno un valor fijo al radio del circulo.
+    const radioCirculo = 3;
+    // creo una constante con la formula del calculo del diametro de un circulo, 
+    const diametroCirculo = radioCirculo * 2;
+    // creo una constante a la cual le asigno el valor de pi.
+    const PI = 3.1415;
+
+/*** Calcular la circunferencia de un circulo con un valor establecido. ***/
+    const circunferencia = diametroCirculo * PI;
+
+/*** Calcular el area de un circulo con un valor establecido. ***/
+    // la formula para calcular el area es radio^2 (al cuadrado), para reemplazar el signo elevado ^ utilizo ** .
+    const areaCirculo = (radioCirculo ** 2) * PI;
+
+    console.log({
+        radioCirculo,
+        diametroCirculo,
+        PI,
+        circunferencia,
+        areaCirculo,
+    });
+
+/*** Funcion para calcular todas las medidas de un circulo. ***/
+    function calcularCirculo(radio) {
+        const diametro = radio * 2;
+        //const radioAlCuadrado = radio ** 2;
+        //Math es un objeto incorporado que tiene propiedades y métodos para funciones y constantes matemáticas. No es un objeto de función.
+        //Puedo encontrar todos los metodos de Math en la documentacion oficial: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+        //la funcion pow recibe dos argumentos, el numero base y el valor de la potencia.
+        const radioAlCuadrado = Math.pow(radio, 2);
+        
+        return {
+            //ahora puedo utilizar el valor de PI directamente utilizando Math y el metodo .PI y asi no es necesario crear una variable con su valor.
+            circunferencia: diametro * Math.PI,
+            area: radioAlCuadrado * Math.PI,
+            //ya que el metodo de PI trae muchos decimales, se puede limitar la cantidad de decimales utilizando toFixed() y entre los parentesis la cantidad de decimales.
+            //area: radioAlCuadrado * Math.PI.toFixed(10),
+        };
+    }
+
+    console.groupEnd('Círculo');
+
