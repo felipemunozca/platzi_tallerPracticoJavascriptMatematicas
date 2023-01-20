c3227e125817c440299f34ddf17998af97ba9806
Felipe Munoz <analista.felipe.m@gmail.com>
2023-01-20 04:11:43 -0300

    Clase 11 terminada.

---
+++ assets/js/estadisticas.js
@@ -0,0 +1,17 @@
+/**
+ * La moda
+ * Consiste en encontrar el valor que se repite la mayor cantidad de veces en una lista de valores.
+ * La moda no solo se puede trabajar con números, sino que aplica tambien para cualquier tipo de dato.
+ */
+
+/**
+ * La mediana
+ * Es el valor que se ubica en la mitad de la lista de valores, cuando la lista es impar.
+ * Cuando la lista es par, entonces se toman los dos valores centrales, se suman y se divide entre dos.
+ */
+
+/**
+ * Promedio
+ * El nombre técnico es media aritmética.
+ * Se calcula sumando todos los valores y luego dividiendo esa suma por la cantidad de valores.
+ */
