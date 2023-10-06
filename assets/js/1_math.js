1ae17a60419295ed8760204bdf9a03098fdf987c
Felipe Muñoz <analista.felipe.m@gmail.com>
2023-10-06 02:35:25 -0300

    Se crea la pagina math.html con sus respectivo CSS e interacciones JS.

--- assets/js/1_math.js
+++ assets/js/1_math.js
@@ -1,12 +1,13 @@
 /**
  * CLASE 2: Fórmulas matemáticas en JavaScript
  * 
- * Se crea una variable para calcular el perimetro de un cuadrado con un valor establecido. En la consola del navegador escribir perimetroCuadrado y la respuesta
- *      deberia ser 20.
- * Se crea una variable para calcular el area de un cuadrado con un valor establecido. En la consola del navegador escribir areaCuadrado y la respuesta deberia ser 25.
- * En el caso en que el usuario no sepa los nombres de las variables no podra imprimirlos, asi que utilizando un consola.log como un objeto, se imprimiran los
- *      resultados de forma automatica de todas las operaciones cuando se abra la consola.
- * Se crean tres variable para calcular el perimetro de un triangulo con un valor establecido.
+ * Se crea una variable para calcular el perímetro de un cuadrado con un valor establecido. En la consola del navegador escribir 
+ *      perimetroCuadrado y la respuesta debería ser 20.
+ * Se crea una variable para calcular el area de un cuadrado con un valor establecido. En la consola del navegador escribir 
+ *      areaCuadrado y la respuesta debería ser 25.
+ * En el caso en que el usuario no sepa los nombres de las variables no podrá imprimirlos, asi que utilizando un consola.log como 
+ *      un objeto, se imprimirán los resultados de forma automática de todas las operaciones cuando se abra la consola.
+ * Se crean tres variable para calcular el perímetro de un triangulo con un valor establecido.
  * Se crea una variable para calcular el area de un triangulo con un valor establecido.
  */
 
@@ -44,19 +45,22 @@ console.log({
 /**
  * CLASE 2 [Continuación]
  * 
- * Se crea una funcion para calcular todas las medidas de un triangulo.
- * Utilizando un return se logra devolver ambos valores del triangulo utilizando una sola funcion.
- * En la consola del navegador, escribir el nombre de la funcion y se pasa cuatro valores, calcularMedidasTriangulo(6, 6, 4, 5.5) la respuesta en la consola 
- *      sera: area = 11 y perimetro = 16.
+ * Se crea una función para calcular todas las medidas de un triangulo.
+ * Utilizando un return se logra devolver ambos valores del triangulo utilizando una sola función.
+ * En la consola del navegador, escribir el nombre de la función y se pasa cuatro valores, calcularMedidasTriangulo(6, 6, 4, 5.5) 
+ *      la respuesta en la consola sera: 
+ *      area = 11 y perimetro = 16.
  * 
- * Se crea una funcion para calcular todas las medidas de un cuadrado.
- * Utilizando un return se logra devolver ambos valores del cuadrado utilizando una sola funcion.
- * En la consola del navegador, escribir el nombre de la funcion y se pasa un valor, calcularMedidasCuadrado(12) la respuesta en la consola sera: 
+ * Se crea una función para calcular todas las medidas de un cuadrado.
+ * Utilizando un return se logra devolver ambos valores del cuadrado utilizando una sola función.
+ * En la consola del navegador, escribir el nombre de la función y se pasa un valor, calcularMedidasCuadrado(12) la respuesta en 
+ *      la consola sera: 
  *      area = 144 y perimetro = 48.
  * 
- * Una forma de ordenar las impresiones del console.log por categorias es utilizar los metodos group y groupEnd. Para utilizar esta opcion de desarrollo, se 
- *      utilizan las palabras reservadas .group() y .groupEnd() y dentro de estos eventos console debe ir la misma palabra dentro de comillas simples ' ' y 
- *      que haga mencion al tema a ordenar, por ejemplo 'cuadrado', 'circulo'.
+ * Una forma de ordenar las impresiones del console.log por categorías es utilizar los métodos "group" y "groupEnd". 
+ * Para utilizar esta opción de desarrollo, se utilizan las palabras reservadas .group() y .groupEnd() y dentro de estos eventos 
+ *      console debe ir la misma palabra dentro de comillas simples ' ' y que haga mención al tema a ordenar, por ejemplo 
+ *      'cuadrado', 'circulo'.
  */
 
 function calcularMedidasTriangulo(lado1, lado2, base, altura) {
@@ -97,13 +101,13 @@ console.groupEnd('Cuadrado');
 /**
  * CLASE 3: Math en JavaScript
  * 
- * Se crea una nueva agrupacion de console, esta vez llamada Círculo.
- * Se crean tres variables, una constante a la que se le asigna un valor fijo al radio del circulo, una constante con la formula del calculo del diametro de un circulo, 
- *      y finalmente, una constante a la cual se le asigna el valor de pi.
+ * Se crea una nueva agrupación de console, esta vez llamada Círculo.
+ * Se crean tres variables, una constante a la que se le asigna un valor fijo al radio del circulo, una constante con la formula del 
+ *      calculo del diámetro de un circulo, y finalmente, una constante a la cual se le asigna el valor de PI.
  * 
  * Se crea una variable para calcular la circunferencia de un circulo con un valor establecido.
- * Se crea una variable para calcular el area de un circulo con un valor establecido. La formula para hacer el calculao es: radio^2 (al cuadrado), para reemplazar el
- *      signo elevado ^ se utiliza ** y luego este valor se multiplica por PI.
+ * Se crea una variable para calcular el area de un circulo con un valor establecido. La formula para hacer el calculo es: 
+ *      radio^2 (al cuadrado), para reemplazar el signo elevado ^ se utiliza ** y luego este valor se multiplica por PI.
  */
 console.group('Círculo')
 
@@ -126,13 +130,15 @@ console.log({
 /**
  * CLASE 3 [Continuación]
  * 
- * Se crea una funcion para calcular la circunferencia y el area de un circulo.
+ * Se crea una función para calcular la circunferencia y el area de un circulo.
  * Dentro de JavaScript existe el objeto "Math" el cual tiene propiedades y métodos para funciones y constantes matemáticas.
- * DOCUMENTACION OFICIAL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+ * DOCUMENTACIÓN OFICIAL: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  * 
- * El metodo .pow() se utiliza para calcular potencias. Este metodo recibe dos argumentos: el numero base y el valor de la potencia.
- * Ya que se esta utilizando Math, no es necesario crear una variable para asignarle el valor de PI, sino que se puede usar la propiedad .PI que trae Math.
- * El metodo de PI trae muchos decimales, se puede limitar la cantidad de decimales utilizando toFixed() y entre los parentesis la cantidad de decimales.
+ * El método .pow() se utiliza para calcular potencias. Este método recibe dos argumentos: el numero base y el valor de la potencia.
+ * Ya que se esta utilizando Math, no es necesario crear una variable para asignarle el valor de PI, sino que se puede usar la 
+ *      propiedad .PI que trae Math.
+ * El método de PI trae muchos decimales, se puede limitar la cantidad de decimales utilizando toFixed() y entre los paréntesis 
+ *      la cantidad de decimales a mostrar.
  */
 function calcularCirculo(radio) {
     const diametro = radio * 2;
@@ -151,11 +157,12 @@ console.groupEnd('Círculo');
 /**
  * CLASE 4: Reto: altura de un triángulo
  * 
- * Se crea una funcion para calcular la altura de un triangulo isosceles. En un triangulo isosceles dos lados son iguales, uno diferente, el cual siempre es la base.
- * La formula para el calculo es: altura (h) = raiz cuadrada (lados ** 2 - (base ** 2) /4)
- * En la consola del navegador, escribir el nombre de la funcion, y luego pasarle dos parametros. Primero probar si funciona el condicional de error, escribir
- *      calcularAlturaTriangulo(6, 6) y deberia aparecer el mensaje de error. Luego se debe probar con otros parametros, calcularAlturaTriangulo(6, 4) y la altura 
- *      sera de 5,6568...
+ * Se crea una función para calcular la altura de un triangulo isosceles. En un triangulo isosceles dos lados son iguales, uno 
+ *      diferente, el cual siempre es la base.
+ * La formula para el calculo es: altura (h) = raíz cuadrada (lados ** 2 - (base ** 2) /4)
+ * En la consola del navegador, escribir el nombre de la función, y luego enviarle dos parámetros. Primero probar si funciona el 
+ *      condicional de error, escribir calcularAlturaTriangulo(6, 6) y debería aparecer el mensaje de error. 
+ *      Luego se debe probar con otros parámetros, calcularAlturaTriangulo(6, 4) y la altura sera de 5,6568...
  */
 function calcularAlturaTriangulo(lados, base) {
     if (lados == base) {
@@ -163,4 +170,141 @@ function calcularAlturaTriangulo(lados, base) {
     } else {
         return Math.sqrt((lados ** 2) - ( (base ** 2) ) / 4 );
     }
+}
+
+
+/*** Variables para la interacción del HTML con JS ***/
+const formCalcularCuadrado = document.querySelector('#formCalcularCuadrado');
+const inputCuadrado = document.querySelector('#inputCuadrado');
+const resultadoPerimetroCuadrado = document.querySelector('#resultadoPerimetroCuadrado');
+const resultadoAreaCuadrado = document.querySelector('#resultadoAreaCuadrado');
+
+const formCalcularTriangulo = document.querySelector('#formCalcularTriangulo');
+const inputTrianguloLadoUno = document.querySelector('#inputTrianguloLadoUno');
+const inputTrianguloLadoDos = document.querySelector('#inputTrianguloLadoDos');
+const inputTrianguloBase = document.querySelector('#inputTrianguloBase');
+const inputTrianguloAltura = document.querySelector('#inputTrianguloAltura');
+const resultadoPerimetroTriangulo = document.querySelector('#resultadoPerimetroTriangulo');
+const resultadoAreaTriangulo = document.querySelector('#resultadoAreaTringulo');
+
+const formCalcularCirculo = document.querySelector('#formCalcularCirculo');
+const inputCirculo = document.querySelector('#inputCirculo');
+const resultadoDiametroCirculo = document.querySelector('#resultadoDiametroCirculo');
+const resultadoCircunferenciaCirculo = document.querySelector('#resultadoCircunferenciaCirculo');
+const resultadoAreaCirculo = document.querySelector('#resultadoAreaCirculo');
+
+const formCalcularIsosceles = document.querySelector('#formCalcularIsosceles');
+const inputLadosIsosceles = document.querySelector('#inputLadosIsosceles');
+const inputBaseIsosceles = document.querySelector('#inputBaseIsosceles');
+const resultadoAlturaIsosceles = document.querySelector('#resultadoAlturaIsosceles');
+
+
+/*** Eventos ***/
+formCalcularCuadrado.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (inputCuadrado.value == "") {
+        alert('Debe agregar el valor del lado del cuadrado.')
+        inputCuadrado.focus();
+        return;
+    }
+
+    calcularMedidasCuadrado();
+});
+
+formCalcularTriangulo.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (inputTrianguloLadoUno.value == "" ) {
+        alert('Debe agregar el valor del lado uno del triangulo.')
+        inputTrianguloLadoUno.focus();
+        return;
+    }
+    if (inputTrianguloLadoDos.value == "" ) {
+        alert('Debe agregar el valor del lado dos del triangulo.')
+        inputTrianguloLadoDos.focus();
+        return;
+    }
+    if (inputTrianguloBase.value == "" ) {
+        alert('Debe agregar el valor de la base del triangulo.')
+        inputTrianguloBase.focus();
+        return;
+    }
+    if (inputTrianguloAltura.value == "" ) {
+        alert('Debe agregar el valor de la altura del triangulo.')
+        inputTrianguloAltura.focus();
+        return;
+    }
+
+    calcularMedidasTriangulo();
+});
+
+formCalcularCirculo.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (inputCirculo.value == "") {
+        alert('Debe agregar el valor del radio del circulo.')
+        inputCirculo.focus();
+        return;
+    }
+
+    calcularMedidasCirculo();
+});
+
+formCalcularIsosceles.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (inputLadosIsosceles.value == '') {
+        alert('Debe agregar el valor de los lados del triangulo.')
+        inputLadosIsosceles.focus();
+        return;
+    }
+    if (inputBaseIsosceles.value == '') {
+        alert('Debe agregar el valor de la base del triangulo.')
+        inputBaseIsosceles.focus();
+        return;
+    }
+
+    if (inputLadosIsosceles.value == inputBaseIsosceles.value) {
+        alert('Estas medidas no son las de un triangulo Isosceles.')
+        return;
+    }
+
+    calcularMedidasIsosceles();
+});
+
+/*** Funciones ***/
+calcularMedidasCuadrado = () => {
+    const perimetroCuadrado = parseInt(inputCuadrado.value) * 4;
+    const areaCuadrado = parseInt(inputCuadrado.value) * parseInt(inputCuadrado.value);
+
+    resultadoPerimetroCuadrado.innerHTML = `El perímetro es de: <strong>${perimetroCuadrado}</strong>`;
+    resultadoAreaCuadrado.innerHTML = `El área es de: <strong>${areaCuadrado}</strong>`;
+}
+
+calcularMedidasTriangulo = () => {
+    const perimetroTriangulo =  parseInt(inputTrianguloLadoUno.value) + 
+                                parseInt(inputTrianguloLadoDos.value) + 
+                                parseInt(inputTrianguloBase.value);
+    const areaTriangulo = (parseInt(inputTrianguloBase.value) * parseInt(inputTrianguloAltura.value)) / 2;
+
+    resultadoPerimetroTriangulo.innerHTML = `El perímetro es de: <strong>${perimetroTriangulo}</strong>`;
+    resultadoAreaTriangulo.innerHTML = `El área es de: <strong>${areaTriangulo}</strong>`;
+}
+
+calcularMedidasCirculo = () => {
+    const diametroCirculo = parseInt(inputCirculo.value) * 2;
+    const circunferenciaCirculo = diametroCirculo * Math.PI;
+    const areaCirculo = (Math.pow(parseInt(inputCirculo.value), 2) * Math.PI)
+
+    resultadoDiametroCirculo.innerHTML = `El diámetro es de: <strong>${diametroCirculo}</strong>`;
+    resultadoCircunferenciaCirculo.innerHTML = `La circunferencia es de: <strong>${circunferenciaCirculo.toFixed(3)}</strong>`;
+    resultadoAreaCirculo.innerHTML = `El área es de: <strong>${areaCirculo.toFixed(3)}</strong>`;
+}
+
+calcularMedidasIsosceles = () => {
+    const alturaIsosceles = Math.sqrt((parseInt(inputLadosIsosceles.value) ** 2) - 
+                                      (parseInt(inputBaseIsosceles.value) ** 2) / 4);
+
+    resultadoAlturaIsosceles.innerHTML = `La altura es de: <strong>${alturaIsosceles.toFixed(3)}</strong>`;
 }
